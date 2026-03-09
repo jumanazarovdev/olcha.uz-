595de23aed7562c36e504c2b69cd50f7a11a96aa
Jumanazarov <jumanazarovazizbek710@gmail.com>
2026-03-10 02:49:24 +0500

    Add commit

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="shadow">
+        <div className="container-fluid py-4">
+          <img src="https://olcha.uz/_nuxt/plus.lRzD4Jf7.png" alt="" />
+        </div>
+      </header>
+
+      <section id="products" className="py-3">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productsCord">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/ji6Hq3G4tIYbQQnAoKsECBXqHF1q0jPBrk31h0QgnEjdW7MgvqBhERJ3NjqO.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName">
+                  Telefon
+                </a>
+                <p className="price">2 290 000 sum</p>
+                <p className="priceMonth"> 2 290 000 sum / mec</p>
+
+                <p className="title"> iphone</p>
+                <a href="#" className="btn ">
+                  Savatcha
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productsCord">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/ji6Hq3G4tIYbQQnAoKsECBXqHF1q0jPBrk31h0QgnEjdW7MgvqBhERJ3NjqO.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName">
+                  Telefon
+                </a>
+                <p className="price">2 290 000 sum</p>
+                <p className="priceMonth"> 2 290 000 sum / mec</p>
+
+                <p className="title"> iphone</p>
+                <a href="#" className="btn ">
+                  Savatcha
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productsCord">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/ji6Hq3G4tIYbQQnAoKsECBXqHF1q0jPBrk31h0QgnEjdW7MgvqBhERJ3NjqO.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName">
+                  Telefon
+                </a>
+                <p className="price">2 290 000 sum</p>
+                <p className="priceMonth"> 2 290 000 sum / mec</p>
+
+                <p className="title"> iphone</p>
+                <a href="#" className="btn ">
+                  Savatcha
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productsCord">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/ji6Hq3G4tIYbQQnAoKsECBXqHF1q0jPBrk31h0QgnEjdW7MgvqBhERJ3NjqO.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName">
+                  Telefon
+                </a>
+                <p className="price">2 290 000 sum</p>
+                <p className="priceMonth"> 2 290 000 sum / mec</p>
+
+                <p className="title"> iphone</p>
+                <a href="#" className="btn ">
+                  Savatcha
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productsCord">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/ji6Hq3G4tIYbQQnAoKsECBXqHF1q0jPBrk31h0QgnEjdW7MgvqBhERJ3NjqO.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName">
+                  Telefon
+                </a>
+                <p className="price">2 290 000 sum</p>
+                <p className="priceMonth"> 2 290 000 sum / mec</p>
+
+                <p className="title"> iphone</p>
+                <a href="#" className="btn ">
+                  Savatcha
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productsCord">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/cdn_1/supplier/stores/1/2026-02-25/ji6Hq3G4tIYbQQnAoKsECBXqHF1q0jPBrk31h0QgnEjdW7MgvqBhERJ3NjqO.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName">
+                  Telefon
+                </a>
+                <p className="price">2 290 000 sum</p>
+                <p className="priceMonth"> 2 290 000 sum / mec</p>
+
+                <p className="title"> iphone</p>
+                <a href="#" className="btn ">
+                  Savatcha
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
